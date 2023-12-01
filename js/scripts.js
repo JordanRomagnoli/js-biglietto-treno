5391bb9b6ac5323929cdc59a854910e8d09fe039
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-01 15:57:45 +0100

    - ADD output

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ if (isNaN(userKm)){
     alert("inserisci un valore valido");
 };
 
-const priceForKm = (0.21 * parseInt(userKm));
+const priceForKm = (0.21 * userKm);
 
 
 let userPrice = (priceForKm).toFixed(2);
@@ -13,8 +13,8 @@ console.log("Il prezzo per l'utente è : " + userPrice);
 
 // ------------------------------------------------------------------ OK
 
-let userAge = prompt('Quanti anni hai ?');
-console.log("l'età dell'utente è : " + userAge);
+let userAge = parseInt(prompt('Quanti anni hai ?'));
+console.log("l'età dell'utente è : " + userAge, typeof userAge);
 
 if (isNaN(userAge)){
     alert("inserisci un valore valido");
@@ -23,27 +23,44 @@ if (isNaN(userAge)){
 // ------------------------------------------------------------------ OK
 
 
-let discount20 = ((parseInt(userPrice) * 20) / 100);
+let discount20 = (((userPrice) * 20) / 100);
 
-let discount40 = ((parseInt(userPrice) * 40) / 100);
+let discount40 = (((userPrice) * 40) / 100);
 
 
 // ------------------------------------------------------------------ OK
 
 
-let userPriceUnder18
+let userPriceUnder18;
+let userPriceOver65;
 
 if (userAge < 18){
-    userPriceUnder18 = (parseInt(userPrice) - parseInt(discount20));
-};
+
+    userPriceUnder18 = ((userPrice) - (discount20)).toFixed(2);
+
+}else if ((userAge > 65)) {
+
+    userPriceOver65 = ((userPrice) - (discount40)).toFixed(2);
+}
 
 console.log("Il prezzo per l'utente under 18 è : " + userPriceUnder18);
 
+console.log("Il prezzo per l'utente over 65 è : " + userPriceOver65);
 
-let userPriceOver65;
+// ------------------------------------------------------------------ OK
 
-if (userAge > 65){
-    userPriceOver65 = (parseInt(userPrice) - parseInt(discount40));
+
+ if (userAge < 18){
+     document.getElementById('user-price').innerHTML = ` ${userPriceUnder18}€ `;
+}
+
+else if (userAge > 65){
+    document.getElementById('user-price').innerHTML = ` ${userPriceOver65}€ `;
+}
+
+else if (userAge >= 18){
+    document.getElementById('user-price').innerHTML = ` ${userPrice}€ `;
 };
 
-console.log("Il prezzo per l'utente over 65 è : " + userPriceOver65);
+// ------------------------------------------------------------------ OK
+
